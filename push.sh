d55973239d7522dc3dc3f9f7d9eba1193311d242
Wandile Mawelela <wandile1997@gmail.com>
2023-01-25 17:50:27 +0200

    ''

--- push.sh
+++ push.sh
@@ -1,9 +1,24 @@
-!/bin/bash
+#! /bin/bash
 
-echo “Running command from” $PWD
-cd $PWD
-git add .
-echo “Enter commit message: “
-git commit -am “$commitMessage”
-git push
-echo “Strata!”
+function addcommitpush () {
+
+current=$(git branch | grep "*" | cut -b 3-)
+
+message=\'"$@"\'
+git add -A && git commit -a -m "$message"
+
+echo "Where to push?"
+read -i "$current" -e branch
+
+echo "You sure you wanna push? (y/n)"
+read -i "y" -e yn
+
+if [ "$yn" = y ]; then
+  git push origin "$branch"
+else
+  echo "Strata!"
+fi
+
+}
+
+addcommitpush $1
